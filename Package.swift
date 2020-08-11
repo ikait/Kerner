6e2ea47ef9b8704b7b3f471252522ebda14b02cd
Taishi Ikai <t@ikai.tokyo>
2020-08-11 17:14:08 +0900

    Restruct project files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,7 +18,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Kerner",
-            dependencies: [],
-            path: "Kerner")
+            dependencies: [])
     ]
 )
